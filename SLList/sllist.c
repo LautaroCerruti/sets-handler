bcb42969576b3145b195d9a10764213822160ac4
LautaroCerruti <lautarocerruti7@gmail.com>
2020-08-18 05:14:49 -0300

    PLS GOD END MY SUFFERING

--- SLList/sllist.c
+++ SLList/sllist.c
@@ -11,32 +11,34 @@ SLList sllist_create() {
 
 void sllist_destroy(SLList list, Destroy function) {
   SLNode *auxNode = list;
-  if (list) {
+  if (list) { // si existe la lista
     list = list->next;
-    for (; list != NULL; list = list->next) {
-      function(auxNode->data);
-      free(auxNode);
+    for (; list != NULL; list = list->next) { // mientras haya elementos en la lista
+      function(auxNode->data);  // liberamos el del nodo anterior
+      free(auxNode); // liberamos el nodo anterior
       auxNode = list;
     }
-    function(auxNode->data);
-    free(auxNode);
+    function(auxNode->data); // liberamos el dato del ultimo nodo
+    free(auxNode); // liberamos el nodo
   }
 }
 
 SLList sllist_insert_with_replace(SLList list, void *data, Compare compare, Destroy function) {
   SLNode *node = NULL;
   SLList aux = list;
-  if (!list) {
+  if (!list) { // si no existe la lista
     node = malloc(sizeof(SLNode));
     node->data = data;
     node->next = NULL;
-    return node;
+    return node; // creamos un nuevo nodo con el dato y lo retornamos
   }
-  for (; !compare(list->data, data) && list->next != NULL; list = list->next);
-  if (compare(list->data, data)) {
-    function(list->data);
-    list->data = data;
-  } else {
+  // mientras el nodo de la lista no evalue en verdadero en la comparacion y la lista no haya llegado al final se mueve por la lista
+  for (; !compare(list->data, data) && list->next != NULL; list = list->next); 
+  if (compare(list->data, data)) { // si la comparacion evalua verdadera
+    function(list->data); // borramos el dato guardado
+    list->data = data; // gurdamos el nuevo dato en este nodo
+  } else { // si no
+    // creamos un nuevo nodo y lo insertamos al final de la lista
     node = malloc(sizeof(SLNode));
     node->data = data;
     node->next = NULL;
@@ -46,6 +48,7 @@ SLList sllist_insert_with_replace(SLList list, void *data, Compare compare, Dest
 }
 
 void* sllist_find(SLList list, void *data, Compare compare){
+  // mientras el nodo de la lista no evalue en verdadero en la comparacion y la lista no haya llegado al final se mueve por la lista
   for (; list != NULL && !compare(list->data, data); list = list->next);
-  return (list ? list->data : NULL);
+  return (list ? list->data : NULL); // si encontro el nodo lo retorna un puntero al mismo, si no retorna NULL
 }
--- interprete.c
+++ interprete.c
@@ -8,15 +8,23 @@
 #define MAX_LINEA 1000
 #define MAX_NOMBRE_CONJUNTO 50
 
+/*
+  check_alphanumeric: char* -> int
+  chequea que todo los caracteres de un string sean alphanumericos
+*/
 int check_alphanumeric(char* string) {
   int ban = 1, i = 0;
-  for (; string[i] != '\0'; ++i){
-    if (!isalnum(string[i]))
-      ban = 0;
+  for (; string[i] != '\0' && ban; ++i){
+    if (!isalnum(string[i])) //si no es alfanumerico el caracter
+      ban = 0; // cambia la bandera a 0
   }
   return ban;
 }
 
+/*
+  hasheo_string: int void -> int
+  Dado el tamanio de la tabla y el string, calcula el hasheo
+*/
 int hasheo_string(int tamanio_tabla, void *valorVoid) {
   unsigned long long int value_hash = 0;
   char* valor =(char*) valorVoid;
@@ -24,10 +32,6 @@ int hasheo_string(int tamanio_tabla, void *valorVoid) {
   for (; valor[i] != '\0'; ++i)
     value_hash += valor[i] * pow(2, (i % 10));
   return (int) (value_hash % tamanio_tabla);
-}
-
-int comparar_conjunto_string(void* conjunto, void* string) {
-  return strcmp(((Conjunto*)conjunto)->nombre, (char*)string) == 0 ? 1 : 0;
 }
 
 /*
@@ -58,18 +62,29 @@ char *leer_cadena(char *string) {
   return aux;                   // Devolvemos un puntero al comienzo de string
 }
 
+/*
+  imprimir: THash char*
+  Busca un conjunto en la tabla hash y lo imprime
+*/
 void imprimir(THash tabla, char* string){
-  if (check_alphanumeric(string)) {
+  if (check_alphanumeric(string)) { // Si no es valido el nombre del cojunto
+    // busca el conjunto en la tabla
     Conjunto* conjunto = tabla_buscar_elemento(tabla, string, hasheo_string, comparar_conjunto_string);
-    if (conjunto)
+    if (conjunto) // si existe
       conjunto_imprimir(*conjunto);
-    else
+    else // si no existe lo informa
       printf("Conjunto no encontrado\n");
   } else {
     printf("Comando incorrecto\n");
   }
 }
 
+/*
+  inserta_conjunto_comprension: THash char* char* -> int
+  Dada la tabla, el string a parsear y el nombre del conjunto,
+  se fija que respete el formato y agrega el conjunto a la tabla.
+  Retorna 1 si hubo un error en la sintaxis, 0 en caso contrario
+*/
 int inserta_conjunto_comprension(THash tabla, char* string, char* nombre_conjunto) {
   Conjunto* conjunto;
   char c = *string, *resto;
@@ -83,7 +98,6 @@ int inserta_conjunto_comprension(THash tabla, char* string, char* nombre_conjunt
   if (*string == ' ')
     ++string;
   numero1 = strtod(string, &resto);
-
   if (string == resto || (*resto != ' ' && *resto != '<'))
     return 1;
   string = resto;
@@ -118,11 +132,18 @@ int inserta_conjunto_comprension(THash tabla, char* string, char* nombre_conjunt
     ++string;
   if (*string != '}')
     return 1;
+  // si el extremo izquiero es mas grande al derecho crea un conjunto vacio, en otro caso, crea el intervalo
   conjunto = numero1 > numero2 ? conjunto_create_empty(nombre_conjunto) : conjunto_create_extension(nombre_conjunto, numero1, numero2);
   tabla = tabla_agregar_elemento(tabla, conjunto, conjunto_hash, conjunto_compara_nombre, conjunto_destroy_conjunto);
   return 0;
 }
 
+/*
+  inserta_conjunto_extension: THash char* char* -> int
+  Dada la tabla, el string a parsear y el nombre del conjunto,
+  se fija que respete el formato y agrega el conjunto a la tabla.
+  Retorna 1 si hubo un error en la sintaxis, 0 en caso contrario
+*/
 int inserta_conjunto_extension(THash tabla, char* string, char* nombre_conjunto) {
   char* resto;
   int numero, bandera = 0;
@@ -146,23 +167,30 @@ int inserta_conjunto_extension(THash tabla, char* string, char* nombre_conjunto)
         bandera = 1;
     }
     if (!bandera) {
-      conjunto = conjunto_agregar_elemento(conjunto, numero);
+      conjunto = conjunto_agregar_elemento(conjunto, numero); // va agregando los elementos a medida que los lee
     }
   }
   if (bandera || (*string =='}' && *(string+1)!='\0')) {
-    conjunto_destroy_conjunto(conjunto);
+    conjunto_destroy_conjunto(conjunto); // si algo fallo destruye el conjunto creado
     return 1;
   } else {
-    conjunto = conjunto_normalize(conjunto);
+    conjunto = conjunto_normalize(conjunto); // si la sintaxis estaba bien, ordena y normaliza el conjunto creado
     tabla = tabla_agregar_elemento(tabla, conjunto, conjunto_hash, conjunto_compara_nombre, conjunto_destroy_conjunto);
+    // lo agrega a la tabla
     return 0;
   }
 }
 
+/*
+  realizar_operacion: THash char char* char* char*
+  Dada una tabla, la operacion a realizar, el nombre del nuevo conjunto, 
+  y los nombres de los conjuntos a operar, realiza la operacion que 
+  corresponde
+*/
 void realizar_operacion(THash tabla, char operacion, char* nombre_primer_conjunto, char* nombre_segundo_conjunto, char* nombre_tercer_conjunto){
   Conjunto *primerConjuntoOperacion = tabla_buscar_elemento(tabla, nombre_segundo_conjunto, hasheo_string, comparar_conjunto_string), *segundoConjuntoOperacion = NULL, *conjuntoResultado;
-  if (operacion != '~')
-    segundoConjuntoOperacion = tabla_buscar_elemento(tabla, nombre_tercer_conjunto, hasheo_string, comparar_conjunto_string);
+  if (operacion != '~') // si la operacion no es complemento
+    segundoConjuntoOperacion = tabla_buscar_elemento(tabla, nombre_tercer_conjunto, hasheo_string, comparar_conjunto_string); // busca el segundo conjunto a operar
   if ((primerConjuntoOperacion && operacion == '~') || (primerConjuntoOperacion && segundoConjuntoOperacion)){
     if (operacion == '~')
       conjuntoResultado = conjunto_complemento(*primerConjuntoOperacion, nombre_primer_conjunto);
@@ -172,8 +200,9 @@ void realizar_operacion(THash tabla, char operacion, char* nombre_primer_conjunt
       conjuntoResultado = conjunto_resta(*primerConjuntoOperacion, *segundoConjuntoOperacion, nombre_primer_conjunto);
     if (operacion == '&')
       conjuntoResultado = conjunto_interseccion(*primerConjuntoOperacion, *segundoConjuntoOperacion, nombre_primer_conjunto);
+  // realiza la operacion correspondiente y agrega el conjunto a la tabla
   tabla = tabla_agregar_elemento(tabla, conjuntoResultado, conjunto_hash, conjunto_compara_nombre, conjunto_destroy_conjunto);
-  } else
+  } else // si no encuentra algun conjunto lo informa
     printf("Conjunto inexistente\n");
 }
 
@@ -181,7 +210,7 @@ int main() {
   char buffer[MAX_LINEA], aux[MAX_NOMBRE_CONJUNTO], *bufferAux, *nombre_primer_conjunto = NULL, *nombre_segundo_conjunto = NULL, *nombre_tercer_conjunto = NULL, operacion;
   int i = 0, error =0;
   buffer[0] = '\0';
-  THash tabla = tabla_hash_crear();
+  THash tabla = tabla_hash_crear(); // crea la tabla
 
 //   printf("|: Union\n");
 //   printf("e: Eliminar\n");
@@ -190,7 +219,7 @@ int main() {
 //   printf("  'CaracterOperacion [Numero1, Numero2]'\n");
 //   printf("dfs, bfs: Mostrar arbol\n");
 //   printf("salir: Finalizar programa\n\n");
-  while (strcmp(leer_cadena(buffer), "salir\0")) {
+  while (strcmp(leer_cadena(buffer), "salir\0")) { // si es distinto de salir
     error = 0;
     nombre_primer_conjunto = NULL;
     nombre_segundo_conjunto = NULL;
@@ -198,10 +227,10 @@ int main() {
     if (buffer[0] == 'i' && buffer[1] == 'm' && buffer[2] == 'p'
       && buffer[3] == 'r' && buffer[4] == 'i' && buffer[5] == 'm'
       && buffer[6] == 'i' && buffer[7] == 'r' && buffer[8] == ' ')
-      imprimir(tabla, buffer + 9);
+      imprimir(tabla, buffer + 9); // si el comando es imprimir ejecuta esta funcion
     else {
       for(i = 0;i<MAX_NOMBRE_CONJUNTO && buffer[i] != '=' && buffer[i] != '\0'; ++i){
-        aux[i] = buffer[i];
+        aux[i] = buffer[i]; // lee el nombre del conjunto resultado
       }
       if (buffer[i] == '=' && i > 0) {
         if(aux[i-1] == ' ')
@@ -220,7 +249,7 @@ int main() {
         ++i;
         if (buffer[i]==' ')
           ++i;
-        if (buffer[i]=='{') {
+        if (buffer[i]=='{') { // si es una creacion de conjunto
           ++i;
           if(isalpha(buffer[i]) && buffer[i] != '-'){
             error = inserta_conjunto_comprension(tabla, buffer+i, nombre_primer_conjunto);
@@ -228,15 +257,13 @@ int main() {
             error = inserta_conjunto_extension(tabla, buffer+i, nombre_primer_conjunto);
           } else
                 error = 1;
-        } else if (buffer[i]=='~') {
+        } else if (buffer[i]=='~') { // si es un complemento
           bufferAux = buffer + (i+1);
           operacion = buffer[i];
-
           for(i = 0;i<MAX_NOMBRE_CONJUNTO && *bufferAux != '\0' && !error; ++bufferAux) {
-            aux[i] = *bufferAux;
+            aux[i] = *bufferAux; // lee el conjunto a operar
             ++i;
           }
-
           if (*bufferAux == '\0' && i > 0 && !error) {
             if(aux[i-1] == ' ')
               aux[i-1] = '\0';
@@ -249,8 +276,6 @@ int main() {
               error = 1;
           } else
             error = 1;
-
-          // Complemento
           if (!error)
             realizar_operacion(tabla, operacion, nombre_primer_conjunto, nombre_segundo_conjunto, nombre_tercer_conjunto);
         } else if (!isalnum(buffer[i]))
@@ -258,7 +283,7 @@ int main() {
         else {
           bufferAux = buffer + i;
           for(i = 0;i<MAX_NOMBRE_CONJUNTO && *bufferAux != '-' && *bufferAux != '&' && *bufferAux != '|' && *bufferAux != '\0' && !error; ++bufferAux) {
-            aux[i] = *bufferAux;
+            aux[i] = *bufferAux; // lee el primer conjunto a operar
             ++i;
           }
 
@@ -282,7 +307,7 @@ int main() {
               ++bufferAux;
 
             for(i = 0;i<MAX_NOMBRE_CONJUNTO && *bufferAux != '\0' && !error; ++bufferAux) {
-              aux[i] = *bufferAux;
+              aux[i] = *bufferAux; // lee el segundo conjunto a operar
               ++i;
             }
             if (*bufferAux == '\0' && i > 0 && !error) {
@@ -304,12 +329,12 @@ int main() {
       }
     }
     if (nombre_segundo_conjunto)
-      free(nombre_segundo_conjunto);
+      free(nombre_segundo_conjunto); // libera la memoria de los nombres de los conjuntos a operar
     if (nombre_tercer_conjunto)
-      free(nombre_tercer_conjunto);
+      free(nombre_tercer_conjunto); // libera la memoria de los nombres de los conjuntos a operar
     if (error)
       printf("Entrada incorrecta\n");
   }
-  tabla_destruir(tabla, conjunto_destroy_conjunto);
+  tabla_destruir(tabla, conjunto_destroy_conjunto); // destruye la tabla
   return 0;
 }
